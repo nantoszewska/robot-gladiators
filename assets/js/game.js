ebaf0cae605f029b76da48d5ab8b10edec597950
nat.anto <nat25pl@gmail.com>
2021-12-10 23:13:49 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,13 +14,13 @@ var fight = function(enemyName) {
             var confirmSkip = window.confirm("Are you sure you'd like to quit?");
             if (confirmSkip) {
                 window.alert(playerName + " has decided to skip this fight. Goodbye!");
-                playerMoney = playerMoney - 10;
+                playerMoney = Math.max(0, playerMoney - 10);
                 console.log("playerMoney", playerMoney);
                 break;
             } 
         }         
-        
-        enemyHealth = enemyHealth - playerAttack;
+        var damage = randomNumber(playerAttack - 3, playerAttack);
+        enemyHealth = Math.max(0, enemyHealth - damage);
         console.log(
             playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
         );
@@ -33,7 +33,8 @@ var fight = function(enemyName) {
             window.alert(enemyName + " still has " + enemyHealth + " health left.");
         }
         
-        playerHealth = playerHealth - enemyAttack;
+        var damage = randomNumber(enemyAttack - 3, enemyAttack);
+        playerHealth = Math.max(0, playerHealth - damage);
         console.log(
             enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
         );
@@ -55,7 +56,7 @@ var startGame = function() {
         if (playerHealth > 0) {
             window.alert("Welcome to Robot Gladiators! " + ( i + 1 ));
             var pickedEnemyName = enemyNames[i];
-            enemyHealth = 50;
+            enemyHealth = randomNumber(40, 60);
             fight(pickedEnemyName);
             if (playerHealth > 0 && i < enemyNames.length - 1) {
                 var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
@@ -99,6 +100,7 @@ var shop = function() {
                 playerMoney = playerMoney - 7; 
             } else {
                 window.alert("You don't have enough money!");
+                shop()
             }
             break;
         case "UPGRADE":
@@ -109,6 +111,7 @@ var shop = function() {
                 playerMoney = playerMoney - 7;  
             } else {
                 window.alert("You don't have enough money!");
+                shop()
             }
             break;
         case "LEAVE":
@@ -120,6 +123,11 @@ var shop = function() {
             shop();
             break;        
     }
+};
+
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min) ;
+    return value;
 };
 
 startGame();
